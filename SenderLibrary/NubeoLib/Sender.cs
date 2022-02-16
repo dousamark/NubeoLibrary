822fc2f09610d6dff7ea88581f7164e1c2f4fc54
Marek Douša <dousamark@gmail.com>
2022-02-16 19:07:21 +0100

    Initial setup of NuGet and Gateway

--- SenderLibrary/NubeoLib/Sender.cs
+++ SenderLibrary/NubeoLib/Sender.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Program")]
+
 namespace NubeoLib
 {
 	class Sender
 	{
+		public static void TestMessage(String user)
+		{
+			Console.WriteLine(user + " says hi");
+		}
 	}
 }
---
+++ SenderLibrary/Program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NubeoLib;
+
+namespace Program
+{
+	class Program
+	{
+		//Entry point
+		static void Main(string[] args)
+		{
+			Sender.TestMessage(args[0]);
+		}
+	}
+}
